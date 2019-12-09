3216e277b02cb34a54b4e64be3b7d3a92d905819
APixelVisuals <apixelvisualsofficial@gmail.com>
2019-12-09 02:51:21 -0800

    Added empty navbar component

---
+++ components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./navBar.scss";
+
+export default class NavBar extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render = () => (
+        <div id="nav-bar">
+
+        </div>
+    )
+};
