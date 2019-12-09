3e2104b7be6cf936f4e979f29cb81441c48da072
APixelVisuals <apixelvisualsofficial@gmail.com>
2019-12-09 01:55:29 -0800

    Init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ozzolum
+Ozzolum is an RPG game that you can play on Discord. Ozzolum is currently in alpha. You can also visit the [bot's repo](https://github.com/APixelVisuals/ozzolum).
---
+++ next.config.js
@@ -0,0 +1,13 @@
+const nextSASS = require("@zeit/next-sass");
+
+module.exports = nextSASS({
+    webpack: config => {
+
+        // Fixes npm packages that depend on `fs` module
+        config.node = {
+            fs: "empty"
+        };
+
+        return config;
+    }
+});
